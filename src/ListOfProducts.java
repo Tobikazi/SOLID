ff3118532d87c7ef66124c798c53ca9937548b15
Tobikazi <117998999+Tobikazi@users.noreply.github.com>
2023-02-26 13:43:03 +0300

    Update ListOfProducts.java

--- src/ListOfProducts.java
+++ src/ListOfProducts.java
@@ -1,5 +1,6 @@
-import java.util.HashMap;
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 import static java.util.Map.entry;
 
@@ -12,8 +13,77 @@ public class ListOfProducts implements IntOfProducts {
     );
 
     @Override
-    public HashMap<String, Integer> getProducts() {
-        return new HashMap<>(products);
-    }
+    public Map<String, Integer> getProducts() {
+        return new Map<>() {
+            @Override
+            public int size() {
+                return 0;
+            }
 
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+
+            @Override
+            public boolean containsKey(Object key) {
+                return false;
+            }
+
+            @Override
+            public boolean containsValue(Object value) {
+                return false;
+            }
+
+            @Override
+            public Integer get(Object key) {
+                return null;
+            }
+
+            @Override
+            public Integer put(String key, Integer value) {
+                return null;
+            }
+
+            @Override
+            public Integer remove(Object key) {
+                return null;
+            }
+
+            @Override
+            public void putAll(Map<? extends String, ? extends Integer> m) {
+
+            }
+
+            @Override
+            public void clear() {
+
+            }
+
+            @Override
+            public Set<String> keySet() {
+                return null;
+            }
+
+            @Override
+            public Collection<Integer> values() {
+                return null;
+            }
+
+            @Override
+            public Set<Entry<String, Integer>> entrySet() {
+                return null;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                return false;
+            }
+
+            @Override
+            public int hashCode() {
+                return 0;
+            }
+        };
+    }
 }
